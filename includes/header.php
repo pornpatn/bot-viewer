104ae5973d17a71a40f1bcd6747ff3aebfe01b20
Pornpat Nikamanon <pornpatn@gmail.com>
2026-03-01 21:10:50 -0800

    Update font for mobile

--- includes/header.php
+++ includes/header.php
@@ -4,17 +4,20 @@ header('Content-Type: text/html; charset=UTF-8');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Birds of Thailand</title>
-    <link rel="stylesheet" href="/assets/css/style.css?v=<?= filemtime(__DIR__ . '/../assets/css/style.css') ?>">
+  <meta charset="UTF-8">
+  <title>Birds of Thailand</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/assets/css/style.css?v=<?= filemtime(__DIR__ . '/../assets/css/style.css') ?>">
 </head>
+
 <body>
 
-<header class="site-header">
-  <div class="container site-header-inner">
-    <a class="site-logo" href="index.php">
-      Birds of Thailand
-    </a>
-</div>
-</header>
+  <header class="site-header">
+    <div class="container site-header-inner">
+      <a class="site-logo" href="index.php">
+        Birds of Thailand
+      </a>
+    </div>
+  </header>
